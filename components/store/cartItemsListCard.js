919b4e5ba3b654f12b922d49e983e658d84eabe0
sourceKing <miraclef60@gmail.com>
2020-11-27 23:07:37 +0100

    Created cartItemsListCard.s

---
+++ components/store/cartItemsListCard.js
@@ -0,0 +1,14 @@
+import {  } from 'react';
+import Card from 'ui/card';
+
+
+export const ItemCard = () => {
+
+  return (
+    <>
+      <Card type="inner">
+        <h4>Making all this stepup</h4>
+      </Card>
+    </>
+  )
+}
