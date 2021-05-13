3344e2c1b336a9e839c604f07820cc362127c7dd
Mimi <miraclef60@gmail.com>
2021-05-13 22:43:44 +0100

    environment variable file fixed

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  env: {
-    API_URL: 'http://localhost:1337',
-  },
-  webpack: config => {
-    config.resolve.alias['components'] = path.join(__dirname, 'components');
-    config.resolve.alias['public'] = path.join(__dirname, 'public');
-    config.resolve.alias['lib'] = path.join(__dirname, 'lib');
-    config.resolve.alias['ui'] = path.join(__dirname, 'node_modules/antd/lib');
+  webpack: (config) => {
+    config.resolve.alias["components"] = path.join(__dirname, "components");
+    config.resolve.alias["public"] = path.join(__dirname, "public");
+    config.resolve.alias["lib"] = path.join(__dirname, "lib");
+    config.resolve.alias["ui"] = path.join(__dirname, "node_modules/antd/lib");
 
     return config;
-  }
-}
+  },
+};
